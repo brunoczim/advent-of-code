78d73faf9b31a4335c422662e2dc8837dd2080de
brunoczim <brunoczim@gmail.com>
2020-12-02 22:45:28 -0300

    printing both parts of day 1

--- 2020/brunoczim/haskell/day1/app/Main.hs
+++ 2020/brunoczim/haskell/day1/app/Main.hs
@@ -1,10 +1,20 @@
 module Main where
 
 import Lib (readEntries, selectEntries, entriesProd)
+import qualified Data.Text.Lazy.IO as TIO
+import Data.Text.Lazy.Builder.Int (decimal)
+import qualified Data.Text.Lazy.Builder as TBuilder
+import qualified Data.Text.Lazy as Text
 
 main :: IO ()
 main = do
   entries <- readEntries
-  let selected = selectEntries 2020 3 entries
-      prod = entriesProd selected
-  print prod 
+  let selected2 = selectEntries 2020 2 entries
+      prod2 = entriesProd selected2
+      selected3 = selectEntries 2020 3 entries
+      prod3 = entriesProd selected3
+      tPrint = TIO.putStrLn . TBuilder.toLazyText . decimal
+  TIO.putStr (Text.pack "Part 1: ")
+  tPrint prod2
+  TIO.putStr (Text.pack "Part 2: ")
+  tPrint prod3
